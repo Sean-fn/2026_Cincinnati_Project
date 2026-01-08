97e432a6ccf2b1ecd0f522b3d6875e0f6c5125e5
Sean-fn <seanfang122@gmail.com>
2026-01-08 21:48:18 +0800

    Update CUDA device settings and model paths in training scripts

--- stage_3_train.sh
+++ stage_3_train.sh
@@ -4,7 +4,7 @@ conda activate M2F2_det
 current_path=$(pwd)
 export PYTHONPATH="$current_path:$PYTHONPATH"
 
-CUDA_NUM=1
+CUDA_NUM=0
 CUDA_VISIBLE_DEVICES=$CUDA_NUM python scripts/merge_lora_weights_deepfake.py \
     --model-base /user/guoxia11/cvlshare/cvl-guoxia11/huggingface/hub/llava-v1.5-7b \
     --model-path ./checkpoints/llava-v1.5-7b-deepfake_stage-2-proj \
